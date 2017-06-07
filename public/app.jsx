d844b0d73c80f1f9c077b797a2b9663b562321d5
aayushis12 <aayushisinha84@gmail.com>
2017-06-08 00:08:13 +0530

    react first setup with components

---
+++ public/app.jsx
@@ -0,0 +1,13 @@
+
+var Example =React.createClass({
+	render: function() {
+		return(
+			<div>
+			<p>Hallo react with component</p></div>);
+	}
+});
+//thank you
+ReactDOM.render(
+<Example />,
+  document.getElementById('app')
+);
