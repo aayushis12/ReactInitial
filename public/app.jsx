1e7b2028d1a88ff68d248ee2709987d29432b323
aayushis12 <aayushisinha84@gmail.com>
2017-06-08 00:11:52 +0530

    second commit

--- public/app.jsx
+++ public/app.jsx
@@ -3,7 +3,7 @@ var Example =React.createClass({
 	render: function() {
 		return(
 			<div>
-			<p>Hallo react with component</p></div>);
+			<p>Hallo react with component with simple applications</p></div>);
 	}
 });
 //thank you
